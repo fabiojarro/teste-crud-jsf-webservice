94ccee956b7e63e68db7250dc08be380fd6ed35a
Fabio Jarro <fabio_jarro@yahoo.com.br>
2016-04-05 11:48:46 -0300

    Alterações para rodar em H2

--- script-ps.sql
+++ script-ps.sql
@@ -1,17 +1,15 @@
-create schema vectorx;
-
-create table vectorx.tbano(
+create table tbano(
 	id serial,
     ano integer not null unique,
     descricao varchar(255),
 	PRIMARY KEY (id)  
 );
-create table vectorx.tbtipo(
+create table tbtipo(
 	id serial primary key,
 	descricao varchar(50) not null unique
 );
 
-create table vectorx.tbdata(
+create table tbdata(
 	id serial,
 	fkano integer,
 	fktipo integer,
@@ -19,20 +17,20 @@ create table vectorx.tbdata(
 	datafim timestamp not null,
 	descricao varchar(50),
 	primary key (id),
-	foreign key (fkano) REFERENCES vectorx.tbano(id),
-	foreign key (fktipo) REFERENCES vectorx.tbtipo(id)
+	foreign key (fkano) REFERENCES tbano(id),
+	foreign key (fktipo) REFERENCES tbtipo(id)
 	
 );
-insert into vectorx.tbano (ano) values (2015);
-insert into vectorx.tbano (ano) values (2016);
-insert into vectorx.tbano (ano) values (2013);
-insert into vectorx.tbano (ano) values (2014);
+insert into tbano (ano) values (2015);
+insert into tbano (ano) values (2016);
+insert into tbano (ano) values (2013);
+insert into tbano (ano) values (2014);
 
-insert into vectorx.tbtipo (descricao) values ('Feriado Nacional');
-insert into vectorx.tbtipo (descricao) values ('Ponto Facultativo');
+insert into tbtipo (descricao) values ('Feriado Nacional');
+insert into tbtipo (descricao) values ('Ponto Facultativo');
 
-insert into vectorx.tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,1,'2015-01-01 00:00:00','2015-01-01 23:59:59','Confraternização universal');
-insert into vectorx.tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-20 00:00:00','2015-02-20 23:59:59','Carnaval');
-insert into vectorx.tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-21 00:00:00','2015-02-21 23:59:59','Carnaval');
-insert into vectorx.tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-22 00:00:00','2015-02-22 14:00:00','Quarta-feira de Cinzas');
-insert into vectorx.tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,1,'2015-11-02 00:00:00','2015-11-02 23:59:59','Finados');
+insert into tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,1,'2015-01-01 00:00:00','2015-01-01 23:59:59','Confraternização universal');
+insert into tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-20 00:00:00','2015-02-20 23:59:59','Carnaval');
+insert into tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-21 00:00:00','2015-02-21 23:59:59','Carnaval');
+insert into tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,2,'2015-02-22 00:00:00','2015-02-22 14:00:00','Quarta-feira de Cinzas');
+insert into tbdata (fkano,fktipo,datainicio,datafim,descricao) values (1,1,'2015-11-02 00:00:00','2015-11-02 23:59:59','Finados');
